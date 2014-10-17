240672f2c64ad5bb41284cb39b766b51a46c661b
Lewis Marshall <lewis@lmars.net>
2014-10-17 01:54:49 +0100

    Add trailing newline to output
    Signed-off-by: Lewis Marshall <lewis@lmars.net>

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ require('../vendor/autoload.php');
 $app = new \Slim\Slim();
 
 $app->get('/', function () {
-    echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname();
+    echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname() . "\n";
 });
 
 $app->run();
